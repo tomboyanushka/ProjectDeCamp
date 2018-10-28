578505d6a779775510de2793c15eb93d95f1f1c3
tomboyanushka <anushkanair20@gmail.com>
2018-10-28 19:01:48 -0400

    Braking [Progress]

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -9,6 +9,8 @@ public class VehicleController : MonoBehaviour
     {
         m_horizontalInput = Input.GetAxis("Horizontal");
         m_verticalInput = Input.GetAxis("Vertical");
+        m_brakingInput = Input.GetKey(KeyCode.Space);
+        
     }
 
     private void Steer()
@@ -43,22 +45,50 @@ public class VehicleController : MonoBehaviour
         _transform.rotation = _quat;
     }
 
+    private void Brake()
+    {
+        if (m_brakingInput)
+        {
+
+
+            frontDriverW.motorTorque = 0;
+            frontPassengerW.motorTorque = 0;
+            //brakeForce += brakeForce * Time.deltaTime;
+            frontDriverW.brakeTorque = brakeForce; //* motorForce;
+            frontPassengerW.brakeTorque = brakeForce; //* motorForce;
+            rearDriverW.brakeTorque = brakeForce; //* motorForce;
+            rearPassengerW.brakeTorque = brakeForce; //* motorForce;
+        }
+        else
+        {
+            brakeForce = 1000;
+            frontDriverW.brakeTorque = 0;
+            frontPassengerW.brakeTorque = 0;
+            rearDriverW.brakeTorque = 0;
+            rearPassengerW.brakeTorque = 0;
+        }
+
+    }
+
     private void FixedUpdate()
     {
         GetInput();
         Steer();
         Accelerate();
         UpdateWheelPoses();
+        Brake();
     }
 
     private float m_horizontalInput;
     private float m_verticalInput;
     private float m_steeringAngle;
+    private bool m_brakingInput = false;
 
-    public WheelCollider frontDriverW, frontPassengerW;
-    public WheelCollider rearDriverW, rearPassengerW;
+    public WheelCollider frontDriverW, frontPassengerW;  //W = Wheel 
+    public WheelCollider rearDriverW, rearPassengerW;    //T = Transform
     public Transform frontDriverT, frontPassengerT;
     public Transform rearDriverT, rearPassengerT;
     public float maxSteerAngle = 30;
     public float motorForce = 50;
+    public float brakeForce = 1000;
 }
