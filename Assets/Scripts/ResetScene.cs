457d2a912d655c4327124efc2bdcae767457bda1
tomboyanushka <anushkanair20@gmail.com>
2018-11-02 16:37:57 -0400

    Reset Scene

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Ground")
+    //    {
+    //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //    }
+    //}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            SceneManager.LoadScene("Scene0");
+        }
+    }
+
+
+}
