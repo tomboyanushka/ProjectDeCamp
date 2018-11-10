c13ff20aff830884fe2abe81897c414b31507b79
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-11-10 18:01:47 -0500

    Compass [Init]

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public Transform carTransform;
+    Vector3 northDirection;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+}
